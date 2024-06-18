f26876f4125a13d87589488529a905437542fe3b
Vivek Kumar <vivekkumar06102002@gmail.com>
2024-06-18 23:28:40 +0530

    calculator

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
-let input = document.querySelectorbyId('inputBOX');
-let button = document.querySelectorAll('buttons');
+let input = document.getElementById('inputBOX');
+let buttons = document.querySelectorAll('button');
 
 let string = "";
+let arr = Array.from(buttons)
+arr.forEach(button =>{
+    button.addEventListener('click',(e) =>{
+    if(e.target.innerHTML == "="){
+  
+
+      string = eval(string);
+      input.value = string; 
+    }
+    else if(e.target.innerHTML =="AC"){
+    string = "";
+    input.value  = string;
+    }
+    else if(e.target.innerHTML == "DEL"){
+     string = string.substring(0,string.length-1)   
+    }
+    else{
+        string += e.target.innerHTML;
+        input.value = string;
+    }
+    })
+    
+  
+})
+
+
+
+   
